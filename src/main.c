44a269a434523b7d0f1cb609d0ed61b44f2b41ac
Nicolas DUCHESNE <niduches@e2r10p8.42.fr>
2020-01-06 13:13:06 +0100

    test for line edition

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: niduches <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/15 19:07:13 by niduches          #+#    #+#             */
-/*   Updated: 2020/01/05 11:41:07 by niduches         ###   ########.fr       */
+/*   Updated: 2020/01/06 13:12:51 by niduches         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,34 @@
 
 #include <curses.h>
 #include <term.h>
+
+void	get_cursor_position(int *col, int *row)
+{
+    char	buff[16];
+	char	*term;
+    size_t	i;
+
+	term = tgetstr("u7", NULL);
+	write(1, term, ft_strlen(term));
+    read(1, buff, 16);
+	*col = 0;
+	*row = 0;
+	i = 2;
+	*row = ft_atoi(&buff[i]) - 1;
+	while (buff[i] >= '0' && buff[i] <= '9')
+		i++;
+	if (buff[i])
+		i++;
+	*col = ft_atoi(&buff[i]) - 1;
+}
+
+void	move_cursor(int col, int row)
+{
+	char	*term;
+
+	term = tgoto(tgetstr("cm", NULL), col, row);
+	write(1, term, ft_strlen(term));
+}
 
 int		main(int ac, char **av, char **arg_env)
 {
@@ -66,7 +94,7 @@ int		main(int ac, char **av, char **arg_env)
 //	write(1, "A", 1);
 //	write(1, "\n", 1);
 	#include <string.h>
-	char	buff[4096];
+	char	buff[128];
 	ssize_t	size;
 	struct termios	termios;
 	char	line[4096];
@@ -75,14 +103,25 @@ int		main(int ac, char **av, char **arg_env)
 	tcgetattr(STDIN_FILENO, &termios);
 	termios.c_lflag &= ~(ICANON | ECHO);
 	tcsetattr(STDIN_FILENO, TCSADRAIN, &(termios));
+
+	int	x = 0;
+	int y = 0;
+	get_cursor_position(&x, &y);
+	move_cursor(0, 0);
+	printf("[%d, %d]\n", x, y);
+	move_cursor(x, y);
+
 	*buff = '\0';
 	while (1)
 	{
+//		write(1, "$> ", 3);
 		bzero(buff, 10);
 		if ((size = read(STDIN_FILENO, buff, 10)) <= 0)
 			return (1);
 		buff[size] = '\0';
 		//printf("buff [%s], %d, %d, %d, %d\n", buff, buff[0], buff[1], buff[2], buff[3]);
+		if (*buff == 4 && buff[1] == '\0')
+			return (0);
 		if (*buff == 27 && !ft_strcmp(buff + 1, "[C"))
 		{
 			term = tgetstr("nd", NULL);
@@ -97,8 +136,10 @@ int		main(int ac, char **av, char **arg_env)
 		}
 		//term = tgetstr("cd", NULL);
 		//write(1, term, ft_strlen(term));
+		move_cursor(0, 0);
 		term = tgetstr("cb", NULL);
 		write(1, term, ft_strlen(term));
+		move_cursor(x, y);
 		strcat(line, buff);
 		write(STDIN_FILENO, line, strlen(line));
 	}
