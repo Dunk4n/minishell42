5f7bc7d7dbd3fbad9264be3351989fa9014ad7a0
jakare <cal_jakare@outlook.com>
2020-01-09 19:48:49 +0200

    cal-hawa: fixed built-in

--- cal-hawa/built_in.c
+++ cal-hawa/built_in.c
@@ -6,7 +6,7 @@
 /*   By: cal-hawa <cal-hawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 14:17:03 by cal-hawa          #+#    #+#             */
-/*   Updated: 2020/01/09 14:34:37 by cal-hawa         ###   ########.fr       */
+/*   Updated: 2020/01/09 19:48:04 by cal-hawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,20 @@ int				launch_built_in(char **argv, t_env *env)
 {
 	int			argc;
 	int			n;
+	char		*blt;
 
+	blt = *argv++;
 	argc = 0;
 	while (argv[argc])
 		argc++;
 	n = -1;
-	if (ft_strcmp(argv[0], "cd") == 0 && (n = 1))
+	if (ft_strcmp(blt, "cd") == 0 && (n = 1))
 		ft_cd(argc, argv, env);
-	else if (ft_strcmp(argv[0], "export") == 0 && (n = 1))
+	else if (ft_strcmp(blt, "export") == 0 && (n = 1))
 		ft_export(argc, argv, env);
-	else if (ft_strcmp(argv[0], "unset") == 0 && (n = 1))
+	else if (ft_strcmp(blt, "unset") == 0 && (n = 1))
 		ft_unset(argc, argv, env);
-	else if (ft_strcmp(argv[0], "exit") == 0 && (n = 1))
+	else if (ft_strcmp(blt, "exit") == 0 && (n = 1))
 		ft_exit(argc, argv, env);
 	return (n);
 }
