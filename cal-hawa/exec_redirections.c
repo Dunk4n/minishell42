1b139a50dbe57b3ae8c782ee38de91a04a59d4dc
jakare <cal_jakare@outlook.com>
2020-01-09 17:49:02 +0200

    cal-hawa: added here-documents

--- cal-hawa/exec_redirections.c
+++ cal-hawa/exec_redirections.c
@@ -6,7 +6,7 @@
 /*   By: cal-hawa <cal-hawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 11:20:40 by cal-hawa          #+#    #+#             */
-/*   Updated: 2020/01/08 14:59:10 by cal-hawa         ###   ########.fr       */
+/*   Updated: 2020/01/09 17:47:05 by cal-hawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ static char		*set_filename(char *file, int *oflag)
 		file[i] = '\0';
 	}
 	if (redir == '<')
-		*oflag = O_RDONLY;
+		*oflag = ft_strncmp(file, "<<", 2) ? O_RDONLY : -1;
 	else
 		*oflag = (ft_strncmp(file, ">>", 2)) ? O_WRONLY | O_CREAT | O_TRUNC :
 O_WRONLY | O_CREAT | O_APPEND;
@@ -56,11 +56,36 @@ static int		redirect_output(char *file_name, int oflag)
 	return (1);
 }
 
-static int		redirect_intput(char *file_name, int oflag)
+static int		here_doc(char *word, int stdin)
 {
+	char		*line;
+	int			pipefd[2];
 
+	if (pipe(pipefd) < 0)
+		return (-1);
+	ft_printf("> ");
+	while (get_next_line(stdin, &line))
+	{
+		if (ft_strcmp(line, word) == 0)
+			break;
+		write(pipefd[1], line, ft_strlen(line));
+		free(line);
+		write(pipefd[1], "\n", 1);
+		ft_printf("> ");
+	}
+	if (close(pipefd[1]) < 0)
+		return (-1);
+	if (dup2(pipefd[0], 0) < 0)
+		return (-1);
+	return (1);
+}
+
+static int		redirect_intput(char *file_name, int oflag, int stdin)
+{
 	int			fd;
 
+	if (oflag == -1)
+		return (here_doc(file_name, stdin));
 	if ((fd = open(file_name, oflag)) < 0)
 		return (-1);
 	if (dup2(fd, 0) < 0)
@@ -74,21 +99,21 @@ int				set_redirections(char **redirs)
 	char		*file_name;
 	int			oflag;
 	int			fd;
+	int			stdin;
 
+	if ((stdin = dup(0)) < 0)
+		return (-1);
 	i = 0;
-	if (!redirs[i])
-		return (1);
-	while (redirs[i + 1])
+	while (redirs[i])
 	{
 		file_name = set_filename(redirs[i], &oflag);
-		if ((fd = open(file_name, oflag, 0664)) < 0)
-			return (-1);
+		if (oflag == O_RDONLY || oflag == -1)
+			fd = redirect_intput(file_name, oflag, stdin);
+		else
+			fd = redirect_output(file_name, oflag);
+		if (fd < 0)
+			return (fd);
 		i++;
 	}
-	file_name = set_filename(redirs[i], &oflag);
-	if (oflag == O_RDONLY)
-		return (redirect_intput(file_name, oflag));
-	else
-		return (redirect_output(file_name, oflag));
 	return (1);
 }
